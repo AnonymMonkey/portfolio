9d2d89ac65112962fbb51831f886b56b1a8725f2
Andino Eichberger <eichberger.andino@gmail.com>
2024-08-20 17:03:46 +0200

    testing material design and optimize form for contact

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
