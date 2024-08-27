13598e9f83ef17f3f09dc3fa255671068e12d0a8
Andino Eichberger <eichberger.andino@gmail.com>
2024-08-27 03:59:43 +0200

    add form logic and add mail.php

--- src/app/main-content/contact/form/form.component.ts
+++ src/app/main-content/contact/form/form.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrl: './form.component.scss',
 })
 export class FormComponent {
+  http = inject(HttpClient);
 
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+    privacy: false,
+  };
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
+    }
+  }
+
+  checkFieldValidity(field: NgModel) {
+    return !field.valid && field.touched && !field.disabled;
+  }
 }
