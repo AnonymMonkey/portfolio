783923c94a096ac02c925a735407a41f7520b7b3
Andino Eichberger <eichberger.andino@gmail.com>
2024-08-29 01:23:08 +0200

    add footer media query

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss',
+  styleUrls: ['./footer.component.scss', './footer-media.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
 export class FooterComponent {}
