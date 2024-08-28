3a2515b7f31b2016c1ce993c9366db2c6f094b27
Andino Eichberger <eichberger.andino@gmail.com>
2024-08-29 01:19:35 +0200

    add projects media query

--- src/app/main-content/portfolio/projects/projects.component.ts
+++ src/app/main-content/portfolio/projects/projects.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss',
+  styleUrls: ['./projects.component.scss', './projects-media.component.scss'],
 })
 export class ProjectsComponent {
   @Input() title!: string;
