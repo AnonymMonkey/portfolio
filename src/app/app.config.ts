6a614d8e4fe923192a0dc84b0125de71ccbf7655
Andino Eichberger <eichberger.andino@gmail.com>
2024-09-20 05:54:16 +0200

    implement translate method and start with header

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,33 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
   ],
 };
--- src/app/main-content/header/header.component.ts
+++ src/app/main-content/header/header.component.ts
@@ -1,11 +1,12 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, TranslateModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss', './header-media.component.scss'],
 })
@@ -14,8 +15,15 @@ export class HeaderComponent implements OnInit {
 
   constructor(
     private viewportScroller: ViewportScroller,
+    private translate: TranslateService,
     private renderer: Renderer2
-  ) {}
+  ) {
+    this.translate.setDefaultLang('en');
+  }
+
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+  }
 
   ngOnInit(): void {
     this.updateActiveSectionOnScroll();
